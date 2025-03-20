3489ffc65f11319e8c305c46dfd31ee77d7780d8
Giancarlo <gianverab@gmail.com>
2025-03-20 02:44:06 -0500

    add interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,37 @@
+export interface Genre {
+    mal_id: number
+    name: string
+}
+
+export interface Studio {
+    mal_id: number
+    name: string
+}
+
+export interface User {
+    uid: string
+    email: string | null
+    displayName: string | null
+}
+export interface Anime {
+    mal_id: number
+    title: string
+    images: {
+        jpg: {
+            image_url: string
+            large_image_url: string
+        }
+    }
+    synopsis: string
+    score: number
+    year: number
+    episodes: number
+    genres: Genre[]
+    studios: Studio[]
+    trailer: {
+        youtube_id: string
+        url: string
+    }
+    status: string
+    airing: boolean
+}
