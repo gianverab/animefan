0fed35a81873c8aa8ae35f5f3d723ed98fc59dcf
Giancarlo <gianverab@gmail.com>
2025-03-20 11:44:45 -0500

    create home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import AnimeGrid from '../components/AnimeGrid'
+import { useTopAnime } from '../hooks/useAnimeData'
+
+const Home: React.FC = () => {
+    const {
+        anime: topAnime,
+        loading: topLoading,
+        error: topError,
+    } = useTopAnime(5)
+
+    return (
+        <div>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+                {/* Top Rated Anime */}
+                <AnimeGrid
+                    anime={topAnime}
+                    loading={topLoading}
+                    error={topError}
+                    title="Top Rated Anime"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Home
