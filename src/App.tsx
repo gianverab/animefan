4cd5a7022570e071f67cbfc8057e1799a419adf4
Giancarlo <gianverab@gmail.com>
2025-04-18 03:27:51 -0500

    update app entry point

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,38 @@ import Navbar from './components/common/Navbar'
 import Footer from './components/common/Footer'
 import Home from './pages/Home'
 import AnimeDetail from './pages/AnimeDetail'
+import Favorites from './pages/Favorites'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import SearchPage from './pages/SearchPage'
 import { AuthProvider } from './contexts/AuthContext'
+import { FavoritesProvider } from './contexts/FavoriteContext'
 
 function App() {
     return (
         <Router>
             <AuthProvider>
-                <div className="bg-background min-h-screen">
-                    <Navbar />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/anime/:id" element={<AnimeDetail />} />
-                        <Route path="/signin" element={<SignIn />} />
-                        <Route path="/signup" element={<SignUp />} />
-                        <Route path="/search" element={<SearchPage />} />
-                    </Routes>
-                </div>
-                <Footer />
+                <FavoritesProvider>
+                    <div className="min-h-screen flex flex-col bg-background text-white">
+                        <Navbar />
+                        <main className="flex-grow">
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route
+                                    path="/anime/:id"
+                                    element={<AnimeDetail />}
+                                />
+                                <Route path="/signin" element={<SignIn />} />
+                                <Route path="/signup" element={<SignUp />} />
+                                <Route
+                                    path="/search"
+                                    element={<SearchPage />}
+                                />
+                            </Routes>
+                        </main>
+                        <Footer />
+                    </div>
+                </FavoritesProvider>
             </AuthProvider>
         </Router>
     )
